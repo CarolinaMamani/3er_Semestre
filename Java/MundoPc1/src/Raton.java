e22678fc26a594ebb7c1efff9c18a21764001ffd
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-04 08:45:46 -0300

    Clases de java 2

--- Java/MundoPc1/src/Raton.java
+++ Java/MundoPc1/src/Raton.java
@@ -2,4 +2,14 @@ public class Raton extends DispositivoEntrada {
     private final int idRaton;
     private static int contadorRatones;
 
+    public Raton(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones; // conteo
+    }
+
+    @Override
+    public String toString() {
+        return "Raton [idRaton=" + idRaton + ", " + super.toString() + "]";
+    }
+
 }
---
+++ Java/MundoPc1/src/Teclado.java
@@ -0,0 +1,15 @@
+public class Teclado extends DispositivoEntrada {
+    private final int idTeclado;
+    private static int contadorTeclados;
+
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado [Teclado=" + idTeclado + ", " + super.toString() + "]";
+    }
+
+}
