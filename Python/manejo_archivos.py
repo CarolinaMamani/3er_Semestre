0a582966901bf43bb74e1f03b71ce24b6f8fbfd0
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-07 16:00:13 -0300

    video2

--- Python/manejo_archivos.py
+++ Python/manejo_archivos.py
@@ -4,6 +4,8 @@ try:
 #usamos el metodo open: para abrir archivos y trabajar en ellos
     archivo = open('prueba.txt', 'w') #sino existe lo crea, sino lo abre
 # w: es write 
+    archivo.write('Programamos con diferentes tipo de archivos, ahora en txt.\n')
+    archivo.write('Como esto terminamos, salto de linea "\n"')
 except Exception as e:
     print(e)
 finally: #siempre se ejecuta
