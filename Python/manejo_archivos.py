f224b6aa6599a25569ad631fb49fd97688a54517
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-07 16:06:45 -0300

    video3

--- Python/manejo_archivos.py
+++ Python/manejo_archivos.py
@@ -2,11 +2,14 @@
 # Declaramos una variable
 try:
 #usamos el metodo open: para abrir archivos y trabajar en ellos
-    archivo = open('prueba.txt', 'w') #sino existe lo crea, sino lo abre
+    archivo = open('prueba.txt', 'w', encoding = 'utf8') #sino existe lo crea, sino lo abre
 # w: es write 
-    archivo.write('Programamos con diferentes tipo de archivos, ahora en txt.\n')
+#utf8: para acentos
+    archivo.write('Programamos con diferentes tipo de archivos, ahora en txt.\n Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo acción\n')
     archivo.write('Como esto terminamos, salto de linea "\n"')
 except Exception as e:
     print(e)
 finally: #siempre se ejecuta
     archivo.close() #se cierra el archivo
+#archivo.write('todo quedo perfecto') : este es un error
