5de147972cfb8dc785e75c9c5a995b23f85a2532
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-07 16:44:30 -0300

    video6

--- Python/leer_archivo.py
+++ Python/leer_archivo.py
@@ -1,4 +1,12 @@
 
 archivo = open('prueba.txt','r', encoding= 'utf8')
 #r : read
-print(archivo.read())
+#print(archivo.read())
+#a: append, anexamos informacion
+#x: para crear archivos
+
+#print(archivo.read(17))
+#print(archivo.read(10)) #continuamos desde la linea anterior
+
+print(archivo.readline()) #lee la primera linea
+print(archivo.readline())
