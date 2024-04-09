56c50597b8e5253ea68f3db309baa9014cd9e2e5
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-09 17:04:12 -0300

    video 6

--- Java/Enumeraciones/src/TestEnumeraciones.java
+++ Java/Enumeraciones/src/TestEnumeraciones.java
@@ -1,10 +1,29 @@
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia 1: " + Dias.LUNES);
-        indicarDiaSemana(Dias.VIERNES); // las enumeraciones se tratan como cadenas
+        // System.out.println("Dia 1: " + Dias.LUNES);
+        // indicarDiaSemana(Dias.VIERNES); // las enumeraciones se tratan como cadenas
         // ahora no se deben utilizar comillas, se accede a traves de el operador de
         // punto
+        System.out.println("Continentes No.4: " + Continentes.EUROPA +
+                "\n     No. de Paises: " + Continentes.EUROPA.getPaises() +
+                "\n     No. de habitantes: " + Continentes.EUROPA.getHabitantes());
+
+        System.out.println("Continentes No.1: " + Continentes.AFRICA +
+                "\n     No. de Paises: " + Continentes.AFRICA.getPaises() +
+                "\n     No. de habitantes: " + Continentes.AFRICA.getHabitantes());
+
+        System.out.println("Continentes No.3: " + Continentes.ASIA +
+                "\n     No. de Paises: " + Continentes.ASIA.getPaises() +
+                "\n     No. de habitantes: " + Continentes.ASIA.getHabitantes());
+
+        System.out.println("Continentes No.2: " + Continentes.AMERICA +
+                "\n     No. de Paises: " + Continentes.AMERICA.getPaises() +
+                "\n     No. de habitantes: " + Continentes.AMERICA.getHabitantes());
+
+        System.out.println("Continentes No.5: " + Continentes.OSEANIA +
+                "\n     No. de Paises: " + Continentes.OSEANIA.getPaises() +
+                "\n     No. de habitantes: " + Continentes.OSEANIA.getHabitantes());
     }
 
     private static void indicarDiaSemana(Dias dias) {
