5103d292ef1660c163f70cc816d2ddc19185afd3
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-09 17:24:36 -0300

    video 8

--- Java/BloquesInicializacion/src/Persona.java
+++ Java/BloquesInicializacion/src/Persona.java
@@ -1,6 +1,7 @@
 
 public class Persona {
     private final int idPersona;
+
     private static int contadorPersonas;
 
     // bloque inicializacion estatico
@@ -8,13 +9,23 @@ public class Persona {
         System.out.println("Ejecucion del bloque estatico");
         ++Persona.contadorPersonas;
         // idPersona = 10; no es un atributo estatico por esto tenemos un error
-
+        // no podemos utilizar this.
     }
 
     // bloque inicializacion NO estatico (contexto Dinamico)
     {
         System.out.println("Ejecucion del bloque NO estatico");
-        this.idPersona = Persona.contadorPersonas++;
-
+        this.idPersona = Persona.contadorPersonas++; // incrementamos el atributo
     }
+
+    // Los bloques de inicializacion se ejecutan ANTES del construcctor
+
+    public Persona() { // constructor
+        System.out.println("Esta es la ejecucion del construcctor");
+    }
+
+    public int getIdPersona() {
+        return this.idPersona;
+    }
+
 }
