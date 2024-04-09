3a3399a91bb1030aae0ccb1a9317d83a253c3754
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-09 15:46:18 -0300

    video 2

--- Java/ArgumentosVariables/src/TestArgumentosVariables.java
+++ Java/ArgumentosVariables/src/TestArgumentosVariables.java
@@ -1,6 +1,16 @@
 public class TestArgumentosVariables {
     public static void main(String[] args) {
+        imprimirNum(3, 4, 5); // nuestro metodo y agregamos numeros
+        imprimirNum(1, 2);
+        variosParametros("Ren-sama ", "Kawashiri ", "27 ", 3, 7);
+    }
 
+    // definir metodos
+    private static void variosParametros(String nombre, String apellido, String edad, int... numeros) { // siempre al
+                                                                                                        // final los
+        // argumentos variables
+        System.out.println("Nombre: " + nombre + "Apellido: " + apellido + "Edad: " + edad);
+        imprimirNum(numeros);
     }
 
     private static void imprimirNum(int... numeros) {// arreglo de tipo entero, los ... indica que la cantidad de
