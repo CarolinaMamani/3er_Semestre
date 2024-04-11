74fce9a12f06d3b0a259b1af50a1301270863d49
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-11 10:30:36 -0300

    Video 2

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -7,11 +7,18 @@ miFuncion(8, 2); //Hosting
 
 function miFuncion(a, b){ //una funcion puede o no tener parametros en () que al momento de ser llamados seran argumentos
     //cuerpo de la funcion que se ejecutara cuando la funcion sea llamada
-    console.log('Sumamos: '+ (a + b));
-    //una funcion siempre retorna, y puede no ser necesario usar la palabra return
+//    console.log('Sumamos: '+ (a + b));
+    //una funcion siempre retorna, y puede no ser necesario usar la palabra return, js lo agrega
+    return a + b;
 }
 
 // llamamos a la funcion
-miFuncion(5,4); //usamos los parametros para llamar argumentos (5,4)
+miFuncion(5,4); //usamos los parametros para pasar argumentos (5,4)
 //podemos llamar a la funcion antes o despues de definirla y no va a suceder ningun error 
 //(hosting) mueve la declaracion al inicio del programa
+
+//usamos una variable para ver el trabajo que esta realizando la funcion
+let resultado = miFuncion(6, 7);
+console.log(resultado) //si quitamos el return del a funcion nos saldra undefined
+
+
