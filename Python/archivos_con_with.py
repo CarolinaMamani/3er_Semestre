7e8d7fbd4a628e4ee5ea8b5f8d4d3b3aa48b7548
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-11 10:11:13 -0300

    Final Python clase 2

--- Python/archivos_con_with.py
+++ Python/archivos_con_with.py
@@ -1,13 +1,19 @@
 # MANEJO DE CONTEXTO WITH: sintaxis simplificada, abre y cierra el archivo automaticamente
 
+from ManejoArchivos2 import ManejoArchivos2
+
+'''
 with open('prueba.txt', 'r', encoding='utf8') as archivo: #manejo de contexto with
     print(archivo.read())
     #no es necesario el try ni el finally
     #context manager
-    '''
+    
     utiliza diferentes metodos: 
         __enter__ este es el que abre
         __exit__ este el que cierra
-    '''
-
     
+'''
+    
+
+with ManejoArchivos2('prueba.txt') as archivo:
+    print(archivo.read())
