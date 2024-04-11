86bf03d9c3dc80a04be158255159a2488d291830
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-11 08:59:38 -0300

    Video 9

--- Python/leer_archivo.py
+++ Python/leer_archivo.py
@@ -9,16 +9,24 @@ archivo = open('C:\\Users\\NYOK\\Documents\\405\\3er_Semestre\prueba.txt','r',
 #print(archivo.read(17))
 #print(archivo.read(10)) #continuamos desde la linea anterior
 
-print(archivo.readline()) #lee la primera linea
-print(archivo.readline())
+#print(archivo.readline()) #lee la primera linea
+#print(archivo.readline())
 
 #Vamos a iterar el archivo, cada una de las lineas
 
 #for linea in archivo:
     #print(linea) Iteramos todos los elementos del archivo linea por linea
     
+'''
 print(archivo.readlines(1))
 print(archivo.readlines()[1])#accedemos al archivo como si fuera una lista separada por comas
-# con [] muestrra la oracion
+# con [] muestrra la oracion '''
 
 
+#Anexamos informacion, copiamos a otro
+archivo2 = open('copia.txt', 'w', encoding='utf8')
+archivo2.write(archivo.read()) #abrimos el archivo
+archivo.close() #cerramos el archivo
+archivo2.close()# cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiar archivos')
