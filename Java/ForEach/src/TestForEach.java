a4d03922aea5b4e852a7db535ee935d2835b74b7
Carolina Mamani <mamanicarolina27@gmail.com>
2024-04-12 16:17:28 -0300

    Clase 3 Java Video 1

---
+++ Java/ForEach/src/TestForEach.java
@@ -0,0 +1,15 @@
+public class TestForEach {
+    public static void main(String[] args) {
+        // for each es un ciclo for, mejorado
+        int edades[] = { 5, 6, 9, 8 };// arreglo de tipo entero con 4 elementos
+
+        // sintaxis del ForEach
+        // definimos una variable que almacena cada uno de los valores
+        // la variable debe coincidir con los valores del arreglo
+        // int edades = int edad
+        for (int edad : edades) {// es un ciclo simplicado, no hay un contador,
+            // no se puede acceder al indice con un foreach
+            System.out.println("edad= " + edad);
+        }
+    }
+}
