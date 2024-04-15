3010a2aebc47904109e016c19ec82f686e76a3b3
Carolina <mamanicarolina27@gmail.com>
2024-04-15 12:11:12 -0300

    Video 4

--- Java/AutoboxingUnboxing/src/TestAutoboxingUnboxing.java
+++ Java/AutoboxingUnboxing/src/TestAutoboxingUnboxing.java
@@ -5,6 +5,13 @@ public class TestAutoboxingUnboxing {
         /*
          * Clases envolventes de tipos primitivos:
          * Enteros int = la clase envolvente es Integer
+         * long = la clase envolvente es Long
+         * float = clase envolvente Float
+         * double = clase envolvente Double
+         * boolean = clase envolvente Boolean
+         * byte = clase envolvente Byte
+         * char = clase envolvente Character
+         * short = clase envolvente Short
          */
         int enteroPrimitivo = 10; // tipo primitivo comun y corriente, solo almacena un valor y no tiene ni
                                   // atributos ni metodos
@@ -14,6 +21,14 @@ public class TestAutoboxingUnboxing {
                              // metodos/funciones
         // si no usamos calculos complejos, podemos usar Integer
         // Al recurrir al operador de punto tenemos acceso a los metodos
-        System.out.println("entero = " + entero.toString()); // ahora 10 no es entero, sino una cadena
+        System.out.println("entero = " + entero.doubleValue()); // AUTOBOXING: covertimos el entero en un doble
+        // ahora 10 no es entero, sino una cadena
+
+        int entero2 = entero; // UNBOXING
+        // se extrae el valor del objeto y se lo asignamos a la variable primitiva
+        // entero 2
+        // ahora ya no es un objeto, es un tipo primitivo
+        System.out.println("entero2 = " + entero2);
+
     }
 }
