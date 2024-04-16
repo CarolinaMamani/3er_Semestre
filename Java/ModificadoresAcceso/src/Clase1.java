595f1dfffb34213a921c89a1a5403e3605b44b3a
Carolina <mamanicarolina27@gmail.com>
2024-04-16 15:21:39 -0300

    Java 3 video 6

--- Java/ModificadoresAcceso/src/Clase1.java
+++ Java/ModificadoresAcceso/src/Clase1.java
@@ -1,5 +1,18 @@
 public class Clase1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // public static void main(String[] args) { // modificador de acceso PUBLIC
+    // puclic significa que la clase se puede utilizar en cualquier parte de nuestro
+    // proyecto
+
+    // Clase modificador de acceso Atributo
+    public String atributoPublic = "Valor atributo public";
+
+    public Clase1() {
+        System.out.println("Constructor public");
     }
+
+    // Metodos:
+    public void metodoPublico() { // cremos metodo publico
+        System.out.println("Metodo publico");
+    }
+
 }
--- Java/ModificadoresAcceso/src/TestModificadoresAcceso.java
+++ Java/ModificadoresAcceso/src/TestModificadoresAcceso.java
@@ -1,5 +1,9 @@
 public class TestModificadoresAcceso {
     public static void main(String[] args) {
+        Clase1 clase1 = new Clase1(); // llamamos al constructor
+        System.out.println("clase 1 = " + clase1.atributoPublic); // accedemos por . al atributo que creamos
+        // muestra lo que hay dentro del atributo
 
+        clase1.metodoPublico();
     }
 }
