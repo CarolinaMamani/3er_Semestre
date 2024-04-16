ef44c80a602eac0492f926920a9aad765f204db8
Carolina <mamanicarolina27@gmail.com>
2024-04-16 19:06:45 -0300

    PY 3 Video 5

--- Python/catalogo_peliculas/test_catalogo_peliculas.py
+++ Python/catalogo_peliculas/test_catalogo_peliculas.py
@@ -1,4 +1,7 @@
 
+from Python.catalogo_peliculas.dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp # vamos a usar esa clase con el nombbre cp
+
 opcion = None
 #creamos un ciclo while
 while opcion != 4:
@@ -10,9 +13,19 @@ while opcion != 4:
         print('3. Eliminar catalogo de peliculas')
         print('4. Salir owe')
         #hacemos la salida y entrada para que el usuario ingrese las opciones
+        
         opcion = int(input('Digite una opcion de menu (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Digite le nombre de la pelicula: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+    
     except Exception as e:
         print(f'Ocurrio un error de tipo: {e}')
         opcion = None
     else: #si presiona 4
-        print('Salimos del programa')
+        print('Salimos del programa...')
