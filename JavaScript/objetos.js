d82338a10e726452a5e64317332a66105c25f2cc
Carolina <mamanicarolina27@gmail.com>
2024-04-17 14:34:44 -0300

    JS 4 Video 1

---
+++ JavaScript/objetos.js
@@ -0,0 +1,13 @@
+// objetos tienen propiedades y metodos
+let x = 10; //variable de tipo primitiva
+console.log(x.lenght); //no encuenta atributos(propiedades)
+
+// Creamos un objeto
+let Persona = { //objeto memomoria con =
+    //propiedades
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'gil@gmail.com',
+    edad: 30
+} 
+console.log(Persona.nombre);
