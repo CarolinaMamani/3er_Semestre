3324d3cac381fe688d33e5ea97c99385a34d28f0
Carolina <mamanicarolina27@gmail.com>
2024-04-20 19:29:03 -0300

    JS 4 video 7

--- JavaScript/objetos.js
+++ JavaScript/objetos.js
@@ -1,6 +1,7 @@
 // objetos tienen propiedades y metodos
 let x = 10; //variable de tipo primitiva
 console.log(x.lenght); //no encuenta atributos(propiedades)
+console.log('Tipos primitivos')
 
 // Creamos un objeto
 let Persona = { //objeto memomoria con =
@@ -21,7 +22,7 @@ console.log(Persona);
 
 //imprimimos el metodo
 console.log(Persona.nombreCompleto());
-
+console.log('Ejecutando con un objeto')
 //Segunda forma de crear un objeto
 let persona2 = new Object(); //debe crear un nuevo objeto en memoria
 persona2.nombre = 'Juan'; //atravez del punto agregamos propiedades al objeto
@@ -29,20 +30,21 @@ persona2.direccion = 'Salida 24';
 persona2.telefono = '2456754';
 console.log(persona2.telefono);
 
-
+console.log('creamos un nuevo objeto')
 //Formas de acceder a pripiedades de los objetos 
 console.log(Persona['apellido']); //accedemos como si fuera un arreglo
-
+console.log('usamos el ciclo for in')
 //for in, y accedemos al objeto como si fuera un arreglo
 for(propiedad in Persona){
     //dos formas
     console.log(propiedad);
     console.log(Persona[propiedad]);
 }
-
+console.log('cambiamos y eliminamos un error')
 //Agregar y eliminar propiedades de un objeto
 //Para agregar propiedad apellida
 Persona.apellida = 'Bernadet'; //cambiamos dinamicamente el valor del objeto
 //Para eliminar
 delete Persona.apellida
 console.log(Persona);
+
