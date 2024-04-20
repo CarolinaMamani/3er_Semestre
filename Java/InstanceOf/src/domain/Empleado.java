3c8b9a54495dad5bbb2d257610d2106401957314
Carolina <mamanicarolina27@gmail.com>
2024-04-20 18:55:12 -0300

    JAVA 4 Video 7

---
+++ Java/InstanceOf/src/domain/Empleado.java
@@ -0,0 +1,37 @@
+package domain;
+
+public class Empleado {
+    // atributos 2
+    protected String nombre;
+    protected double sueldo;
+
+    // constructor publico de la clase padre
+    public Empleado(String nombre, double sueldo) {
+        // instancia de los atributos
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    // metodo para sobreescribir en la clasehija
+    public String obtenerDetaller() {
+        return "Nombre: " + this.nombre + ", Sueldo: " + this.sueldo;
+    }
+
+    // getter and setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+}
---
+++ Java/InstanceOf/src/test/TestInstanceOf.java
@@ -0,0 +1,28 @@
+package test;
+
+import domain.Empleado;
+import domain.Gerente;
+
+public class TestInstanceOf {
+    public static void main(String[] args) {
+        // variables de la clase padre
+        Empleado empleado1 = new Empleado("Juan", 100000);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+
+    }
+
+    // InstanteOf, nos permite preguntar de que tipo es la refencia a la cual esta
+    // apuntando cierta variable (empleado1)
+    // instancia de cierto tipo
+    public static void determinarTipo(Empleado empleado) { // con el metodo imprimir imprimimos datos de la clase padre
+                                                           // e hija
+        if (empleado instanceof Gerente) {
+            System.out.println("Es de tipo Gerente");
+        } else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+        } else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
+    }
+}
