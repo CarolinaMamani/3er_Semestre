5ef6f1b41816fc4bf34176d684086df3e146c0e0
Carolina <mamanicarolina27@gmail.com>
2024-04-21 06:54:44 -0300

    PY 5 Video 1

--- Python/BD/prueba_bd.py
+++ Python/BD/prueba_bd.py
@@ -1,22 +1,22 @@
 import psycopg2 # esto es para poder conectarnos a postrgre
 
-conexion = psycopg2.connect(
-    user = 'postgres',
-    password = 'admin',
-    host = '127.0.0.1',
-    port = '5433',
-    database = 'test_bd'
-)
+conexion = psycopg2.connect(user = 'postgres', password = 'admin', host = '127.0.0.1', port = '5433', database = 'test_bd')
 
-#cursor para permitir ejecutar sentencias en sql
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) #ejecutamos la cadena sentencia
-registros = cursor.fetchall() #metodo fetchall recupera todos los registros de la sentencia que seran una lista
-
-print(registros) #recibimos una lista, pero internamente tenemos tuplas
+try: 
+    with conexion:
+        with conexion.cursor() as cursor: # Este uso de with cierra el recurso del cursor automaticamente
+        #cursor para permitir ejecutar sentencias en sql
+            cursor = conexion.cursor()
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia) #ejecutamos la cadena sentencia
+            registros = cursor.fetchall() #metodo fetchall recupera todos los registros de la sentencia que seran una lista
+            print(registros) #recibimos una lista, pero internamente tenemos tuplas
 #conexion exitosa
-#cerramos la conexion
-cursor.close()
-conexion.close()
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:#cerramos la conexion, para que siempre se ejecute al final
+    conexion.close()
+    
+
+
 
