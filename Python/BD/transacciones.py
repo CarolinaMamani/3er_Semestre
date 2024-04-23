ea8f1e19606fdfe4dcf2dff7b7b928ac556afeba
Carolina <mamanicarolina27@gmail.com>
2024-04-23 15:55:27 -0300

    PY 6 Video 4

--- Python/BD/transacciones.py
+++ Python/BD/transacciones.py
@@ -3,14 +3,17 @@ import psycopg2 as bd
 conexion = bd.connect(user = 'postgres', password = 'admin', host = '127.0.0.1', port = '5433', database = 'test_bd')
 
 try: 
-   conexion.autocommit = False #transacciones, si no hacemos commit entonces no se guardan los cambios
+    #esto no deberia estar
+   # conexion.autocommit = False #transacciones, si no hacemos commit entonces no se guardan los cambios
    cursor = conexion.cursor()
    sentencia = 'INSERT INTO persona(nombre, apellido, email)VALUES(%s, %s, %s)'
    valores = ('Maria','Llanos','maloi@mail.com')
    cursor.execute(sentencia,valores)
+   conexion.commit() #hacemos el commit manualmente
    print('Termina la transanccion')
    
 except Exception as e:
-    print(f'Ocurrio un error: {e}')
+    conexion.rollback() #sino se hacer el comit hace rollback y queda todo como el principio
+    print(f'Ocurrio un error, se hizo un roll back: {e}')
 finally:
     conexion.close()
