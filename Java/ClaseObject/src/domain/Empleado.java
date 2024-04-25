78b6028cd5aaf7941b2f5a551f7bb2f3525a77cf
Carolina <mamanicarolina27@gmail.com>
2024-04-25 12:37:52 -0300

    JV 5 Video 5

--- Java/ClaseObject/src/domain/Empleado.java
+++ Java/ClaseObject/src/domain/Empleado.java
@@ -39,4 +39,34 @@ public class Empleado {
         return "Empleado [nombre=" + nombre + ", sueldo=" + sueldo + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(sueldo);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override // comparacion en contenido EQUALS
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Empleado other = (Empleado) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (Double.doubleToLongBits(sueldo) != Double.doubleToLongBits(other.sueldo))
+            return false;
+        return true;
+    }
+
 }
--- Java/ClaseObject/src/test/TestClaseObejct.java
+++ Java/ClaseObject/src/test/TestClaseObejct.java
@@ -14,17 +14,11 @@ public class TestClaseObejct {
             System.out.println("Tienen una distinta referencia en memoria");
         }
 
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        // TODO Auto-generated method stub
-        return super.equals(obj);
-    }
-
-    @Override
-    public int hashCode() {
-        // TODO Auto-generated method stub
-        return super.hashCode();
+        // ---------
+        if (empleado1.hashCode() == empleado2.hashCode()) {
+            System.out.println("El valor hashCode es igual");
+        } else {
+            System.out.println("El valor hashCode es diferente");
+        }
     }
 }
