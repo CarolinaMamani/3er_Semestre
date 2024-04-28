49e0e3dd5e1228974e0cca4eabc70bfeafda2171
Carolina <mamanicarolina27@gmail.com>
2024-04-28 15:51:26 -0300

    JS 6 Video 4

--- JavaScript/5-clases.js
+++ JavaScript/5-clases.js
@@ -14,10 +14,20 @@ class Persona{
 
 //Metodos GET y SET
     get nombre(){
-        return this._nombre
+        return this._nombre;
     }
 
-   
+    get apellido(){
+        return this._apellido;
+    }
+
+   set nombre(nombre){
+        this._nombre = nombre;
+   }
+
+   set apellido(apellido){
+        this._apellido = apellido;
+   }
 
 }
 
@@ -25,9 +35,20 @@ class Persona{
 //cada objeto maneja sus propios datos
 
 let persona1 = new Persona('Joha','Oter');
+console.log(persona1.nombre); //usamos el .nombre es el get y nos necesario poner ()
+persona1.nombre= 'Anais Cyndi'; // usamos el set para cambiar el nombre
 console.log(persona1.nombre);
 
+console.log(persona1.apellido);
+persona1.apellido= 'Waterson';
+console.log(persona1.apellido);
+
 let persona2 = new Persona('Anya','Forger');
+console.log(persona2.nombre); //get
+persona2.nombre= 'Gumball'; //set
 console.log(persona2.nombre);
 
+console.log(persona2.apellido);
+persona2.apellido= 'Waterson';
+console.log(persona2.apellido);
 
