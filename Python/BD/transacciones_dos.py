2b04d3f91e7e070264b664dd91b3b97aab5a205f
Carolina <mamanicarolina27@gmail.com>
2024-04-28 13:28:16 -0300

    PY 6 Video 5

---
+++ Python/BD/transacciones_dos.py
@@ -0,0 +1,27 @@
+import psycopg2 as bd
+
+conexion = bd.connect(user = 'postgres', password = 'admin', host = '127.0.0.1', port = '5433', database = 'test_bd')
+
+try: 
+
+   conexion.autocommit = False# se inicia la transaccion
+   
+   #nueva insercion de elemento
+   cursor = conexion.cursor()
+   sentencia = 'INSERT INTO persona(nombre, apellido, email)VALUES(%s, %s, %s)'
+   valores = ('Luwrty','Mayri','melu@mail.com')
+   cursor.execute(sentencia,valores)
+   
+   #modificando un elemento
+   sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+   valores = ('Juan','Juares','jcm@mail.com',1)
+   cursor.execute(sentencia,valores)
+   
+   conexion.commit() # se cierra la transaccion
+   print('Termina la transanccion')
+   
+except Exception as e:
+    conexion.rollback() #sino se hacer el comit hace rollback y queda todo como el principio
+    print(f'Ocurrio un error, se hizo un roll back: {e}')
+finally:
+    conexion.close()
