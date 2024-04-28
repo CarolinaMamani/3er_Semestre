2e88ce0f424034f2ca8c5c3c0f528132f5a0ef70
Carolina <mamanicarolina27@gmail.com>
2024-04-28 15:44:42 -0300

    JS 6 Video 3

--- JavaScript/5-clases.js
+++ JavaScript/5-clases.js
@@ -7,17 +7,27 @@ class Persona{
     constructor(nombre, apellido){ //agregamos el constructor para crear un objeto
         //dentro de los parentesis agregamos los parametros
         //dentro del constructor inicializamos los atributos
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre; //ponemos _ para diferenciar el atributo(_nombre) del nombre
+        this._apellido = apellido;
 
     }//sino se define el constructor javascript lo define de manera automatica
+
+//Metodos GET y SET
+    get nombre(){
+        return this._nombre
+    }
+
+   
+
 }
 
 // ahora vamos a crear OBJETOS:
 //cada objeto maneja sus propios datos
 
 let persona1 = new Persona('Joha','Oter');
-console.log(persona1);
+console.log(persona1.nombre);
 
 let persona2 = new Persona('Anya','Forger');
-console.log(persona2);
+console.log(persona2.nombre);
+
+
