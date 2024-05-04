2b3333c88fe5fd631beb00d2d32d89918ec75284
Carolina <mamanicarolina27@gmail.com>
2024-05-04 16:32:52 -0300

    PY 8 Video 4

--- Python/capa_datos_persona/conexion.py
+++ Python/capa_datos_persona/conexion.py
@@ -1,3 +1,6 @@
+import psycopg2 as bd
+from logger_base import log
+import sys
 
 class Conexion:
     _DATABASE = 'test_bd'
@@ -8,3 +11,19 @@ class Conexion:
     _conexion = None
     _cursor = None
     
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host = cls._HOST,
+                                           user = cls._USERNAME,
+                                           password = cls._PASSWORD,
+                                           port = cls._DB_PORT,
+                                           dstabase = cls._DATABASE)
+                log.debug(f'Conexion exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+                sys.exit()
+            else:
+                return cls._conexion
