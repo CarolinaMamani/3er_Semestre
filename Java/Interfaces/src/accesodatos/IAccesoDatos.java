fffe3e2ee57541690d9007c95141ee9bbc023263
Caro <mamanicarolina27@gmail.com>
2024-05-31 23:03:35 -0300

    JV 7 Video 2

--- Java/Interfaces/src/accesodatos/IAccesoDatos.java
+++ Java/Interfaces/src/accesodatos/IAccesoDatos.java
@@ -2,6 +2,18 @@ package accesodatos;
 
 public interface IAccesoDatos { // interface no hereda de la clase Object
 
-    int MAX_REGISTRO = 10;
+    // cualquier atributo en una interface necesita si o si su valor. Ya que no
+    // tiene constructor y todos los metodos son abstractos
+
+    int MAX_REGISTRO = 10; // es una constante, es public, abstracto, final y static
+
+    // Metodo insertar es abstracto y sin cuerpo
+    void insertar();
+
+    void listar();
+
+    void actualizar();
+
+    void eliminar();
 
 }
