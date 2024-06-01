9038b8b0aa273c2d97ed1c118fe0cfdf1d55637a
Caro <mamanicarolina27@gmail.com>
2024-06-01 20:47:00 -0300

    JV 7 Video 6

--- Java/Interfaces/src/accesodatos/test/TestInterfaces.java
+++ Java/Interfaces/src/accesodatos/test/TestInterfaces.java
@@ -4,7 +4,22 @@ import accesodatos.*;
 
 public class TestInterfaces {
     public static void main(String[] args) {
+
+        // Polimorfismo pero en Interfaces(comportamiento):
+
         IAccesoDatos datos = new ImplementacionMysql();
+        // datos.listar();
+
+        // imprimir(datos);
+
+        datos = new ImplementacionOracle();
+        // datos.listar();
+        imprimir(datos);
+    }
+
+    // Metodo generico, recibe un tipo de interface que apunta tanto a mysql como
+    // oracle
+    public static void imprimir(IAccesoDatos datos) {
         datos.listar();
     }
 }
