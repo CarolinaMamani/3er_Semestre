9b44c19567bbe8539d6298248e2a625363047b1b
Caro <mamanicarolina27@gmail.com>
2024-06-02 21:54:50 -0300

    JV 8 Video 4

--- Java/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -2,7 +2,14 @@ package test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        int resultado = 10 / 0;
-        System.out.println("resultado = " + resultado);
+        int resultado = 0;
+        try {
+            resultado = 10 / 0;
+
+        } catch (Exception e) {
+            System.out.println("\nOcurrio un error:");
+            e.printStackTrace(System.out); // se conoce como la pila de excepciones
+        }
+        System.out.println("La variable de resultado tiene como valor : " + resultado);
     }
 }
