7da446557eae8ed5183b28c2fa4eaa30ff222db1
Caro <mamanicarolina27@gmail.com>
2024-07-02 08:02:30 -0300

    PY 10 video 1 y 2

--- Python/capa_datos_persona/conexion.py
+++ Python/capa_datos_persona/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 import sys
 
@@ -8,38 +8,24 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5433'
     _HOST = '127.0.0.1'
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1 #minimo de conecciones del connection pool
+    _MAX_CON = 5 # no colocar tantos
+    _pool = None
     
+      
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host = cls._HOST,
-                                           user = cls._USERNAME,
-                                           password = cls._PASSWORD,
-                                           port = cls._DB_PORT,
-                                           database = cls._DATABASE)
-                log.debug(f'Conexion exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-                return cls._conexion
+        pass
+    
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+    
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrio correctamente el cursor: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._cursor
- 
+                cls._pool = pool.SimpleConnectionPool()
+    
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass
