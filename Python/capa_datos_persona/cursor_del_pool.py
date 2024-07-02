3886f57ca8e32eff84c3ccd7eac51a8dd9aa5dcd
Caro <mamanicarolina27@gmail.com>
2024-07-02 09:21:42 -0300

    PY 11 Video 4

--- Python/capa_datos_persona/cursor_del_pool.py
+++ Python/capa_datos_persona/cursor_del_pool.py
@@ -12,3 +12,15 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion()
         self._cursor = self._conexion.cursor()
         return self._cursor
+    
+    def __exit__(self, tipo_exception, valor_exception, detalle_exception):
+        log.debug('Se ejecuta el metodo exit')
+        if valor_exception:
+            self._conexion.rollback() # si hay error vuelve atras
+            log.debug(f' Ocurrio una excepcion: {valor_exception}')
+        else:
+            self._conexion.commit() # si ocurre nada, comitea
+            log.debug('Commit de la transaccion')
+        self._cursor.close()#cerramos la conexion del cursor
+        Conexion.liberarConexion(self._conexion) #liberamos
+        
