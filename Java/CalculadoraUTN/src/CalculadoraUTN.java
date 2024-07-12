dce6c9c12b36b1f93d71ec8ee5d522621a30e39d
Caro <mamanicarolina27@gmail.com>
2024-07-12 18:52:32 -0300

    JV 9 Video 3

--- Java/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,11 +1,14 @@
-
+import java.util.Scanner;
 
 public class CalculadoraUTN {
     
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); //OBJETO DE ENTRADA
         System.out.println("******* Aplicacion Calculadora *******");
-        var operando1 = 10;
-        var operando2 = 20;
+        System.out.print("Digite el valor de operando1: "); //println es para que se imprima con salto de linea
+        var operando1 = Integer.parseInt(entrada.nextLine());
+        System.out.print("Digite el valor de operando2: ");
+        var operando2 = Integer.parseInt(entrada.nextLine());
         var resultado = operando1 + operando2;
         System.out.println("resultado = " + resultado);
     }
