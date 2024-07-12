98f97b87e5921ce4e24b54bb9f711e34c3441e26
Caro <mamanicarolina27@gmail.com>
2024-07-12 18:59:33 -0300

    JV 9 Video 4

--- Java/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/CalculadoraUTN/src/CalculadoraUTN.java
@@ -5,11 +5,26 @@ public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in); //OBJETO DE ENTRADA
         System.out.println("******* Aplicacion Calculadora *******");
-        System.out.print("Digite el valor de operando1: "); //println es para que se imprima con salto de linea
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el valor de operando2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+
+        //Mostramos el menu
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operacion a realizar? ");
+        var operacion = Integer.parseInt(entrada.nextLine()); //lee el tipo de operacion que eligio el usuario
+
+        //Ciclo condicional con rango
+        if(operacion >=1 && operacion <=4){
+            System.out.print("Digite el valor de operando1: "); //println es para que se imprima con salto de linea
+            var operando1 = Integer.parseInt(entrada.nextLine());
+
+            System.out.print("Digite el valor de operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+
+        }
     }
 }
