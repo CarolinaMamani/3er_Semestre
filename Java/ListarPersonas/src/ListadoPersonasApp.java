df339388178b70afe7d73c36f50617c0d3f6d666
Caro <mamanicarolina27@gmail.com>
2024-07-13 19:17:23 -0300

    JV 10 Video 6

--- Java/ListarPersonas/src/ListadoPersonasApp.java
+++ Java/ListarPersonas/src/ListadoPersonasApp.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class ListadoPersonasApp {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-    }
+        //definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        //empezamos con el menu
+        var salir = false;
+        while (!salir){
+            mostrarMenu()
+        }//fin de ciclo while
+    }//fin del metodo main
 }
