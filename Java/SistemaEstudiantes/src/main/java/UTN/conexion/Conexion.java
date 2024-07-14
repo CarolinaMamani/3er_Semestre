93a5b5e1a180b8246c81a603b463b98cc9ec4369
Caro <mamanicarolina27@gmail.com>
2024-07-14 16:07:27 -0300

    JV 11 Video 7

--- Java/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
+++ Java/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
@@ -3,6 +3,7 @@ package UTN.conexion;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
     public static Connection getConnection(){
@@ -14,9 +15,13 @@ public class Conexion {
         var password = "admin";
 
         //cargamos la clase del driver de mysql en memoria
-        Class.forName("comm.mysql.cj.jdbc.Driver");
-        conexion = DriverManager.getConnection(url, usuario, password);
-        
+        try{
+            Class.forName("comm.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("ocurrio un error en la conexion"+e.getMessage());
+        }//fin catch
+        return conexion;
 
-    }
+    }//fin metodo de connection
 }
