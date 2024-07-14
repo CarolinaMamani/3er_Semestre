ca3aef3267641fc6381de944d1f5125ec20bbe30
Caro <mamanicarolina27@gmail.com>
2024-07-14 19:35:57 -0300

    JV 12 Video 3

--- Java/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -1,8 +1,9 @@
 package UTN.datos;
 
-import UTN.dominio.Estudiante;
-import com.mysql.cj.x.protobuf.MysqlxPrepare;
+import static UTN.conexion.Conexion.getConnection;
 
+import UTN.conexion.Conexion;
+import UTN.dominio.Estudiante;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ public class EstudianteDAO {
         PreparedStatement ps; //envia la sentendia de sql  hacia la base de datos
         ResultSet rs; //objeto que permite obtener el resultado desde las base de datos
 
-
+        //creamos un objeto de tipo conexion
+        Conexion con = getConnection();
+        String sql = "SELECT * FROM estudiantes2024"
     }
 }
