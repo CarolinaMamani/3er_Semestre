2cbffc103b7a179280ee7d21fb3d43a1d73a43e2
Caro <mamanicarolina27@gmail.com>
2024-07-14 16:16:30 -0300

    JV 12 Video 1

---
+++ Java/SistemaEstudiantes/src/main/java/UTN/dominio/Estudiante.java
@@ -0,0 +1,88 @@
+package UTN.dominio;
+
+public class Estudiante {
+    //sus atributos
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+    //sobrecarga de constructores
+    public Estudiante(){}//constructor vacio
+
+    //para la llave primaria
+    public Estudiante(int idEstudiante){
+        this.idEstudiante= idEstudiante;
+    }
+
+    //para insertar nuevo estudiante
+    public Estudiante(String nombre, String apellido, String telefono, String email){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    //para modificar estudiante
+    public Estudiante(int idEstudiante,String nombre,String apellido, String telefono, String email){
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    //metodos get y set
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "idEstudiante=" + idEstudiante +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
