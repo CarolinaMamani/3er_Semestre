8b4cb8588e0b50fbf67caed5627c5f7756e00fba
Caro <mamanicarolina27@gmail.com>
2024-07-16 14:15:36 -0300

    JV 1 S4 Video 2,3,4

--- Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemasEstudiantesApp.java
+++ Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemasEstudiantesApp.java
@@ -1,13 +1,25 @@
-package UTN;
+package UTN.presentacion;
 
 import UTN.conexion.Conexion;
+import UTN.datos.EstudianteDAO;
+import UTN.dominio.Estudiante;
 
-public class Main {
+import java.util.Scanner;
+
+public class SistemasEstudiantesApp {
     public static void main(String[] args) {
-        var conexion = Conexion.getConnection();
-        if(conexion != null)
-            System.out.println("Te conectaste >3 "+conexion);
-        else
-            System.out.println("Ehrrrr error!");
+        var salir = false;
+        var consola = new Scanner(System.in); // para leer informacion de la consola
+        //se crea una instancia de la clase  servicio, esto lo hacemos fuera el ciclo
+        var estudianteDao = new EstudianteDAO();//Esta instancia debe hacerse una vez
+        while(!salir){
+            try{
+                mostrarMenu(); //este sera el metodo que devolvera un booleano
+                salir = ejecutarOpciones(consola, estudianteDao);//este metodo arroja una excepcion
+            } catch (Exception e){
+                System.out.println("Error error se no se puede dar el menu"+ e.getMessage());
+            }
+        }//fin while
+
     }//fin main
 }// fin de clase
