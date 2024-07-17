c6491c9c8f9a0af703727dd7a98fcb24d6107890
Caro <mamanicarolina27@gmail.com>
2024-07-17 19:39:00 -0300

    JV 1 S4 Video 5

--- Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemasEstudiantesApp.java
+++ Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemasEstudiantesApp.java
@@ -14,7 +14,8 @@ public class SistemasEstudiantesApp {
         var estudianteDao = new EstudianteDAO();//Esta instancia debe hacerse una vez
         while(!salir){
             try{
-                mostrarMenu(); //este sera el metodo que devolvera un booleano
+                mostrarMenu();
+                //este sera el metodo que devolvera un booleano:
                 salir = ejecutarOpciones(consola, estudianteDao);//este metodo arroja una excepcion
             } catch (Exception e){
                 System.out.println("Error error se no se puede dar el menu"+ e.getMessage());
@@ -22,4 +23,37 @@ public class SistemasEstudiantesApp {
         }//fin while
 
     }//fin main
+
+    //Menu
+    private static void mostrarMenu(){
+        System.out.println("""
+                <<<<<<<< SISTEMA DE ESTUDIANTES >>>>>>>
+                1. Pasar lista
+                2. Buscar
+                3. Agregar mas ninios
+                4. Modificar algun nino
+                5. Aniquilar
+                6. Exit
+                
+                Cual deseas mi reina? :D  
+                """);
+    }//fin metodo mostrar menu
+
+//Metodo para ejecutar las opciones, va a regresar un valor boleano, ya que es el que puede modificar el valor de la
+    //la variable salir, si es verdadero termina el ciclo while
+
+    public static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDAO){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion){
+            case 1 ->{
+                System.out.println("Tu lista es: ");
+                
+            }
+            case 2->{
+                System.out.println();
+            }
+        }
+    }
+
 }// fin de clase
